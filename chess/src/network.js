cd4c58e663077968a37f3610ea1c01ca43f1fb2c
stefan-nicolae <84786481+stefan-nicolae@users.noreply.github.com>
2024-01-25 17:07:18 +0200

    Update network.js

--- chess/src/network.js
+++ chess/src/network.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 const PRODUCTION = true
 
-const WebSocketHost = PRODUCTION ? 'wss://chess-app-server-6isz.onrender.com/' : 'ws://localhost:8080'; 
+const WebSocketHost = PRODUCTION ? 'wss://vladolteanu.com/stfn/chess-app' : 'ws://localhost:8080'; 
 
 function useWebSocket(onReceive) {
   const [socket, setSocket] = useState(null);
